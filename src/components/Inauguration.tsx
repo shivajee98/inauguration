d05b89e69423015f2d594f96a20036e22793050b
shivajee98 <ultimateintimater@gmail.com>
2025-02-19 10:04:10 +0530

    resolving

--- src/components/Inauguration.tsx
+++ src/components/Inauguration.tsx
@@ -4,15 +4,14 @@ import { MagicCardDemo } from "./shop";
 
 export function ShineBorderDemo() {
     return (
-        <div className="p-2 md:p-5">
+        <div className="p-2 md:p-5 ">
             <ShineBorder
-                className="relative w-full h-48 md:h-96 rounded-lg md:rounded-full mt-2 bg-slate-800 text-white flex items-center justify-center overflow-hidden"
+                className="relative w-full h-48 md:h-96 rounded-lg mt-2 bg-slate-800 text-white flex items-center justify-center overflow-hidden"
                 color={["#cc0000", "#1a0000", "#FFBE7B"]}
                 borderWidth={8}
-                borderRadius={20}
             >
                 <video
-                    className="w-full h-full object-cover rounded-lg md:rounded-full pointer-events-none"
+                    className="w-full h-full object-cover pointer-events-none"
                     autoPlay
                     loop
                     muted
@@ -22,7 +21,7 @@ export function ShineBorderDemo() {
                 </video>
             </ShineBorder>
 
-            <div className="flex ">
+            <div className="flex  animate-spotlight ">
             <div className="flex flex-col mt-5 md:mt-10">
                 <div className="flex flex-col md:flex-row mt-5 md:mt-10 w-full justify-center gap-4 md:gap-48">
                     <MagicCardDemo text="Pavilion 1" />
@@ -32,7 +31,7 @@ export function ShineBorderDemo() {
                 <div className=" md:mt-14 mx-auto max-w-screen-xl overflow-hidden px-4">
                     <div className="text-3xl text-white font-bold mb-10"><center>Stalls</center></div>
                     <div className="grid ml-10 grid-cols-4 md:grid-cols-8 gap-4 md:gap-20 justify-center">
-                        {[...Array(16)].map((_, i) => (
+                        {[...Array(64)].map((_, i) => (
                             <span 
                                 key={i}
                                 className="w-3 h-3 mt-4 mb-2 md:w-5 md:h-5 bg-red-500 rounded-full animate-emit delay-150"
@@ -49,6 +48,7 @@ export function ShineBorderDemo() {
                 <div className="flex flex-col-reverse">
                 <MagicCardDemo text="Buyer Zone" />
                 <MagicCardDemo text="Join Discussion" />
+                <MagicCardDemo text="Visit Exhibition" />
                 </div>
             </div>
             </div>
